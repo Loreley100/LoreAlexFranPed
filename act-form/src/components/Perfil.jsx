38e294c3492420171bfad12212e00145d751a4d9
Manu0905 <20233tn164@utez.edu.mx>
2025-03-15 00:29:32 -0600

    Pantalla de perfil completa con comentarios

--- act-form/src/components/Perfil.jsx
+++ act-form/src/components/Perfil.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import { useLocation, useNavigate } from "react-router-dom"; // Para recibir informacion del otro componente
 
 export default function Perfil() {
-  const location = useLocation();
-  const userData = location.state || {}; // Se recibe la información
+  let navigate = useNavigate() //Variable para navegar entre componentes
+  const location = useLocation(); // declarar una variable par obtener la informacion
+  const data = location.state || {}; // Se recibe la información
 
   return (
     <div>
-      <p>Nombre: {userData.nombre}</p>
-      <p>Apellidos: {userData.apellidoP} {userData.apellidoM}</p>
-      <p>Edad: {userData.edad}</p>
-      <p>Telefono: {userData.tel}</p>
-      <p>Contraseña: {userData.pass}</p>
+      {/* Pantalla de perfil de usuario */}
+      <h1>Perfil de Usuario</h1>
+      {/* Mostrar el nombre */}
+      <p>Nombre: {data.name}</p>
+      {/* Mostrar ambos apellidos */}
+      <p>Apellidos: {data.lastName} {data.surName}</p>
+      {/* Mostrar la edad */}
+      <p>Edad: {data.age}</p>
+      {/* Mostrar el correo */}
+      <p>Correo: {data.email}</p>
+      {/* Mostrar la contraseña */}
+      <p>Contraseña: {data.pass}</p>
+      {/* Boton para regresar al formulario */}
+      <button onClick={()=> navigate('/')}>Regresar a formulario</button>
     </div>
   )
 }
