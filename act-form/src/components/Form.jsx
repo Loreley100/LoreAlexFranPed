2d50cf463fdc966f5bae068810a21ae2f9bd180a
Flollo22 <20233tn148@utez.edu.mx>
2025-03-15 14:31:44 -0600

    Agregando nuevos componentes y estilos

---
+++ act-form/src/components/Form.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useNavigate, useParams } from 'react-router-dom'
+import './../Form.css'
+
+export default function Form() {
+    //funcion para redirigir
+    let navigate=useNavigate();
+
+
+    
+//declaracion o dorma que debe de tener nuestro formulario
+    const schema = yup.object().shape({
+        name: yup.string("solo ingresar letras").required("El nombre es requerido"),
+        lastName: yup.string("solo ingresar letras").required("El apellido materno es requerido"),
+        surName: yup.string("solo ingresar letras").required("El apellido paterno es requerido"),
+        email: yup.string().email("debe tener formato de email").required("El email es requerido"),
+        age: yup.number("solo ingresar numeros")
+        .min(18,"La edad minima debe de ser 18")
+        .integer("Debes de ingresar un num entero")
+        .required("La edad es requerida")
+        .typeError("Error en el tipo de dato"),
+        telephone: yup.number("solo ingresar numeros")
+        .integer("Debes de ingresar un num entero")
+        .required("El telefono es requerido")
+        .typeError("Error en el tipo de dato"),
+        pass: yup.string().required("La contraseña es requerida").min(4,"La contraseña debe de al menos 4 chars").max(10,"Max 10 chars"),
+        comfirmpass: yup.string().oneOf([yup.ref('pass'), null],"La contraseña no coincide")
+    })      
+
+    const {register, handleSubmit, formState:{errors}} = useForm({
+        resolver: yupResolver(schema)
+    })
+    function onSubmit(data) {
+        //{ state: { userData: obj } }
+        navigate("/login", {state: data } )
+        console.log(data)
+    }
+    
+    return (
+    <div>
+        <h1>Formulario</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder=" your name" {...register("name")} />
+            <p>{errors.name?.message}</p>
+            <input type="text" placeholder=" your surName" {...register("surName")} />
+            <p>{errors.name?.message}</p>
+            <input type="text" placeholder=" your lastName" {...register("lastName")} />
+            <p>{errors.name?.message}</p>
+            <input type="text" placeholder=" your email" {...register("email")}/>
+            <p>{errors.email?.message}</p>
+            <input type="number" placeholder=" your age" {...register("age")}/>
+            <p>{errors.age?.message}</p>
+            <input type="number" placeholder=" your telephone" {...register("telephone")}/>
+            <p>{errors.telephone?.message}</p>
+            <input type="password" placeholder=" your password" {...register("pass")}/>
+            <p>{errors.pass?.message}</p>
+            <input type="password" placeholder=" your password again" {...register("comfirmpass")}/>
+            <p>{errors.comfirmpass?.message}</p>
+            <input className='button' type="submit" />
+        </form>
+    </div>
+  )
+}
---
+++ act-form/src/components/Login.jsx
@@ -0,0 +1,64 @@
+import { useRef } from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { data, useLocation, useNavigate } from "react-router-dom"; // Para recibir informacion del otro componente
+
+import './../Login.css'
+
+
+export default function Login() {
+    const navigate = useNavigate();
+    let location = useLocation();
+    const data = location.state // Se recibe la informacion del otro componente
+
+    const numIntentos = useRef(0)
+
+    const schema = yup.object().shape({ // Se hace una declaracion de que es lo que se quiere
+        email: yup.string().required("Ingrese su correo").email("Formato de email incorrecto"),
+        pass: yup.string().required("La contraseña es requerida")
+            .min(4, "Debe de tener al menos 4 caracteres")
+            .max(10, "Maximo 10 caracteres"),
+    })
+    
+    const { register, handleSubmit, formState: {errors}} = useForm({ // formState es para mostrar los errores
+        resolver: yupResolver(schema)
+    })
+
+    function onSubmit(sigIn){
+        // Si los datos son correctos y el numero de intentos es menor que 3 se redirige a la pagina principal
+        if( sigIn.email === data.email && sigIn.pass === data.pass && numIntentos.current < 3){
+            console.log(data)
+            navigate("/perfil", { state: data });
+            numIntentos.current = 0
+        }
+        else{
+            numIntentos.current++
+            alert("Contraseña o correo incorrecto, intento: " + numIntentos.current)
+        }
+    }
+
+
+    return (
+        <div>
+            <div>
+                <h1>Login</h1>
+                <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label htmlFor="">Correo electronico: </label>
+                        <input type="text" placeholder="loreuwu@gmail.com" {...register("email")} />
+                        { errors.email && <p className='error'>{errors.email?.message}</p> }
+                    </div>
+                    <div>
+                        <label htmlFor="">Contraseña: </label>
+                        <input type="password" placeholder="Password" {...register("pass")}/>
+                        { errors.pass && <p className='error'>{errors.pass?.message}</p> }
+                    </div>
+                    <input className='button' type="submit" value="Ingresar" />
+                </form>
+            </div>
+        </div>
+    )
+}
+
